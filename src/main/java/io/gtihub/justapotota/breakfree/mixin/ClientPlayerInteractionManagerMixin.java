791c580df08301467bbeee3ecdb08560cc875dfa
JustAPotota <justapotota.dev@gmail.com>
2023-07-17 19:42:53 -0500

    Update to 1.20.1

--- src/main/java/io/gtihub/justapotota/breakfree/mixin/ClientPlayerInteractionManagerMixin.java
+++ src/main/java/io/gtihub/justapotota/breakfree/mixin/ClientPlayerInteractionManagerMixin.java
@@ -1,14 +1,18 @@
 package io.gtihub.justapotota.breakfree.mixin;
 
 import net.minecraft.client.network.ClientPlayerInteractionManager;
+import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(ClientPlayerInteractionManager.class)
 public class ClientPlayerInteractionManagerMixin {
-    @ModifyVariable(method = "isCurrentlyBreaking(Lnet/minecraft/util/math/BlockPos;)Z", at = @At("STORE"))
-    private boolean bl(boolean original) {
-        return true;
+    @Shadow private ItemStack selectedStack;
+
+    @ModifyVariable(method = "isCurrentlyBreaking", at = @At("STORE"))
+    private ItemStack stack(ItemStack stack) {
+        return this.selectedStack;
     }
 }
